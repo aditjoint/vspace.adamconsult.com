bb7002e07f8df42086b85a93f8b1da5ceea4e9ac
Lalitendu Samantaray <aditjoint@gmail.com>
2025-10-13 20:15:20 +0530

    Update sw.js
    Signed-off-by: Lalitendu Samantaray <aditjoint@gmail.com>

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-const CACHE = 'signage-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/sender.html','/playlists.json'])));
+const CACHE_NAME = 'adam-signage-v1';
+const urlsToCache = ['index.html','sender.html','player.html','receiver.html','manifest.json','playlists.json'];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
