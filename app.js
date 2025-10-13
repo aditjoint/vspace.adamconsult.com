b3a08948aaab05ba6d9ac128a2bbcb24086f9921
Lalitendu Samantaray <aditjoint@gmail.com>
2025-10-13 08:43:13 +0530

    Update app.js
    Signed-off-by: Lalitendu Samantaray <aditjoint@gmail.com>

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+// Fullscreen
+document.getElementById("fullscreenBtn").addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
+
+// Picture-in-Picture
+document.getElementById("pipBtn").addEventListener("click", async () => {
+  const videos = document.querySelectorAll("video");
+  const activeVideo = videos[0];
+  try {
+    if (document.pictureInPictureElement) {
+      await document.exitPictureInPicture();
+    } else {
+      await activeVideo.requestPictureInPicture();
+    }
+  } catch (error) {
+    alert("PiP not supported in this browser.");
+  }
+});
 
