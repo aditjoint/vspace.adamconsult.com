1613494ef250c192dfef043a0f3a004b1a96fb17
Lalitendu Samantaray <aditjoint@gmail.com>
2025-10-14 07:18:49 +0530

    Update sw.js
    Signed-off-by: Lalitendu Samantaray <aditjoint@gmail.com>

--- sw.js
+++ sw.js
@@ -1,39 +1,52 @@
-const CACHE_NAME = 'adam-signage-v1';
-const urlsToCache = [
-  'index.html',
-  'admin.html',
-  'sender.html',
-  'receiver.html',
-  'player.html',
-  'playlist.json',
-  'layout.json',
-  'manifest.json'
+const CACHE_NAME = 'adam-signage-v3';
+const DATA_FILES = [
+  '/content-library.json',
+  '/devices.json',
+  '/layout.json',
+  '/playlist.json',
+  '/schedules.json',
+  '/zones.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/admin.html',
+          '/sender.html',
+          '/receiver.html',
+          '/player.html',
+          '/manifest.json',
+          '/sw.js',
+          ...DATA_FILES
+        ]);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
+  // Always fetch data files from network first
+  if (event.request.url.includes('.json')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          // Update cache
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put(event.request, responseToCache));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+  
+  // Cache-first for other requests
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
